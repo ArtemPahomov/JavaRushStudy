1fbd83663d55ee286820453228fc2d3893c87885
ArtemPahomov <artem.pahomov989@gmail.com>
2021-02-08 18:30:59 +0300

    commit 08.02.20

---
+++ src/com/test/Types.java
@@ -0,0 +1,22 @@
+package com.test;
+
+public class Types {
+    static int intVar;
+    static double doubleVar;
+    static Double Double;
+    static boolean booleanVar;
+    static Object ObjectVar;
+    static Exception ExceptionVar;
+    static String StringVar;
+
+    public static void main(String[] args) {
+
+        System.out.println(intVar);
+        System.out.println(doubleVar);
+        System.out.println(Double);
+        System.out.println(booleanVar);
+        System.out.println(ObjectVar);
+        System.out.println(ExceptionVar);
+        System.out.println(StringVar);
+    }
+}
