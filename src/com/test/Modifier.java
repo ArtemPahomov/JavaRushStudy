a3d3a051d8f8f73be88f339f5b61e47084e52339
ArtemPahomov <artem.pahomov989@gmail.com>
2021-02-09 14:32:21 +0300

    09.02.21

---
+++ src/com/test/Modifier.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Modifier {
+    public Modifier() {
+    }
+    private Modifier(int i) {
+    }
+    protected Modifier(double d) {
+    }
+     Modifier(short n) {
+    }
+}
---
+++ src/com/test/SubModifier.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class SubModifier extends Modifier {
+    public SubModifier(double d) {
+        super(d);
+    }
+
+    protected SubModifier(short n) {
+        super(n);
+    }
+
+    SubModifier() {
+    }
+}
