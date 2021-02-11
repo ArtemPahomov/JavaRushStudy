476dbec8da13a11896e45f944e36adf3f528277b
ArtemPahomov <artem.pahomov989@gmail.com>
2021-02-11 21:01:39 +0300

    11.02.21

--- src/com/test/LiteTask.java
+++ src/com/test/LiteTask.java
@@ -1,24 +1,18 @@
 package com.test;
 
 public class LiteTask {
-    class Clock implements Runnable
-    {
-        public void run()
-        {
-            Thread current = Thread.currentThread();
 
-            while (!current.isInterrupted())
-            {
-                try
-                {
-                    Thread.sleep(1000);
-                }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-                System.out.println("Tik");
-            }
+    public static class Test extends Thread{
+        @Override
+        public void run() {
+            System.out.println("My name is " + getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Test thread = new Test();
+            thread.start();
         }
     }
 }
