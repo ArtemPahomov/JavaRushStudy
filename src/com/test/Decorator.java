ffc06fcbfaf89d080534bda52522d6f7932c3b00
ArtemPahomov <artem.pahomov989@gmail.com>
2021-02-19 21:00:02 +0300

    19.02.21

--- src/com/test/Decorator.java
+++ src/com/test/Decorator.java
@@ -1,40 +1,12 @@
 package com.test;
 
 public class Decorator {
+    public String s;
+
     public static void main(String[] args) {
-        new Thread(new DecoratorRunnableImpl(new DecoratorMyRunnableImpl(new RunnableImpl()))).start();
+
     }
-
-    public static class RunnableImpl implements Runnable {
-        @Override
-        public void run() {
-            System.out.println("RunnableImpl body");
-        }
-    }
-
-    public static class DecoratorRunnableImpl implements Runnable {
-        private Runnable component;
-
-        public DecoratorRunnableImpl(Runnable component) {
-            this.component = component;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("DecoratorRunnableImpl body ");
-            component.run();
-        }
-    }
-    public static class DecoratorMyRunnableImpl implements Runnable{
-        private Runnable component;
-        public DecoratorMyRunnableImpl(RunnableImpl runnable) {
-            this.component = runnable;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("decoratorMyRunnableImpl body");
-            component.run();
-        }
+    private static class Test2{
+        private static final String NAME="test";
     }
 }
